d01314d7d2550b7b97010094e373bd04ae7f7811
Yuri <yuri.hondo@sony.com>
2022-06-26 23:12:15 +0900

    Implment overlay feature

--- app/src/main/java/com/example/systemoverlaycomposesample/OverlayService.kt
+++ app/src/main/java/com/example/systemoverlaycomposesample/OverlayService.kt
@@ -3,10 +3,12 @@ package com.example.systemoverlaycomposesample
 import android.content.Context
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.view.View
 import android.view.WindowManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
@@ -16,7 +18,10 @@ import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewTreeLifecycleOwner
 import androidx.lifecycle.ViewTreeViewModelStoreOwner
 import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
 import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import com.example.systemoverlaycomposesample.ui.theme.Typography
 
 class OverlayService : LifecycleService(), SavedStateRegistryOwner {
 
@@ -45,34 +50,51 @@ class OverlayService : LifecycleService(), SavedStateRegistryOwner {
         }
     }
 
+    private val savedStateRegistryController = SavedStateRegistryController.create(this)
+
+    override val savedStateRegistry: SavedStateRegistry
+        get() = savedStateRegistryController.savedStateRegistry
+
+    override fun onCreate() {
+        super.onCreate()
+        savedStateRegistryController.performRestore(null)
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when (intent?.action) {
-            START_OVERLAY -> {}
-            STOP_OVERLAY -> {}
+            START_OVERLAY -> handleStartOverlay()
+            STOP_OVERLAY -> handleStopOverlay()
             else -> {}
         }
         return super.onStartCommand(intent, flags, startId)
     }
 
-    private fun handleStartOverlay() {
-        val composeView = ComposeView(this)
-        composeView.setContent {
-            Text(
-                text = "Hello",
-                color = Color.Black,
-                fontSize = 50.sp,
-                modifier = Modifier
-                    .wrapContentSize()
-                    .background(Color.Green)
-            )
+    private val composeView: View by lazy {
+        ComposeView(this).apply {
+            setViewTreeSavedStateRegistryOwner(this@OverlayService)
+            setContent {
+                Text(
+                    text = "Hello, World!",
+                    color = Color.Black,
+                    fontSize = 50.sp,
+                    modifier = Modifier.wrapContentSize(),
+                    style = Typography.headlineLarge
+                )
+            }
         }
-        ViewTreeLifecycleOwner.set(composeView, this)
-        ViewTreeViewModelStoreOwner.set(composeView) {ViewModelStore()}
-        ViewTreeSavedStateRegistryOwner.set(composeView) {SavedStateRegistry()}
-        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
     }
 
-    override val savedStateRegistry: SavedStateRegistry
-        get() = TODO("Not yet implemented")
+    private val windowManager: WindowManager by lazy {
+        getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    }
+
+    private fun handleStartOverlay() {
+        ViewTreeLifecycleOwner.set(composeView, this)
+        ViewTreeViewModelStoreOwner.set(composeView) { ViewModelStore() }
+        windowManager.addView(composeView, overlayParams)
+    }
+
+    private fun handleStopOverlay() {
+        windowManager.removeView(composeView)
+    }
 }
